aca4ea2925fee48825474cf67b45d2f54810b132
Amanda <akjackson1992@yahoo.com>
2017-02-13 16:01:17 -0500

    trying to add everything

---
+++ OverripePantry/app/src/main/java/com/example/amanda/overripepantry/Inventory.java
@@ -0,0 +1,30 @@
+package com.example.amanda.overripepantry;
+//http://stackoverflow.com/questions/31844919/what-is-the-best-way-to-create-a-data-table-in-android
+//Android Programming The Big Nerd Ranch Guide 2nd edition pg. 177 -> RecyclerView
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Inventory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
