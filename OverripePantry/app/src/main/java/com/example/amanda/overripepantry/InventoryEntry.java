25a3d2d8117d4bf42a31b9fe430bbd1302104639
Amanda <akjackson1992@yahoo.com>
2017-02-27 18:04:40 -0500

    Basic Inventory Form

--- OverripePantry/app/src/main/java/com/example/amanda/overripepantry/InventoryEntry.java
+++ OverripePantry/app/src/main/java/com/example/amanda/overripepantry/InventoryEntry.java
@@ -1,4 +1,8 @@
 package com.example.amanda.overripepantry;
+/**
+ * https://code.tutsplus.com/tutorials/android-essentials-creating-simple-user-forms--mobile-1758
+ *
+ */
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -6,6 +10,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class InventoryEntry extends AppCompatActivity {
 
@@ -17,6 +26,31 @@ public class InventoryEntry extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void enterItem(View button) {
+        final EditText itemField = (EditText) findViewById(R.id.editItemName);
+        String itemName = itemField.getText().toString();
+
+        final EditText brandField = (EditText) findViewById(R.id.editNameBrand);
+        String brandName = brandField.getText().toString();
+
+        final EditText quantityField = (EditText) findViewById(R.id.editQuantity);
+        double quantity = Double.parseDouble(quantityField.getText().toString());
+
+        final EditText unitField = (EditText) findViewById(R.id.editQuantityUnit);
+        String unit = unitField.getText().toString();
+
+        final EditText dateField = (EditText) findViewById(R.id.editExpirationDate);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/YYYY");
+        Date expDate;
+        try {
+            expDate = dateFormat.parse(dateField.getText().toString());
+        } catch(ParseException e) {
+            System.out.print("Error in the Date.");
+            expDate = null;
+        }
+
     }
 
 }
