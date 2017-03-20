48792afa0056f4eedc084bd9ffb029302eebe61f
Francisco Fantl <franfantl@gmail.com>
2017-03-20 17:58:45 -0400

    Added database

---
+++ OverripePantry/app/src/main/java/com/example/amanda/overripepantry/InventoryContract.java
@@ -0,0 +1,84 @@
+package com.example.amanda.overripepantry;
+
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+
+public class InventoryContract {
+    public static final String CONTENT_AUTHORITY = "com.example.amanda.database";
+
+    private static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_INVENTORY = "inventory";
+    public static final String PATH_PRODUCTS = "products";
+    public static final String PATH_GROCERYLISTS = "grocerylist";
+    public static final String PATH_GROCERY = "grocery";
+
+    public static class ProductEntry implements BaseColumns {
+        // Content URI represents the base location for the table
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_INVENTORY).build();
+
+        public static final String CONTENT_TYPE =
+                "vnd.android.cursor.dir/" + CONTENT_URI  + "/" + PATH_INVENTORY;
+        public static final String CONTENT_ITEM_TYPE =
+                "vnd.android.cursor.item/" + CONTENT_URI + "/" + PATH_INVENTORY;
+
+        public static final String TABLE_NAME = "products";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_BRAND = "brand";
+        public static final String COLUMN_NAME_WEIGHT = "weight";
+        public static final String COLUMN_NAME_UPC = "upc";
+        public static final String COLUMN_NAME_EXPDATE = "expDate";
+        public static final String COLUMN_NAME_ENTRYDATE = "entryDate";
+
+
+
+
+        public static Uri buildProductUri(long id){
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+    public static class GroceryListEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_GROCERYLISTS).build();
+
+        public static final String CONTENT_TYPE =
+                "vnd.android.cursor.dir/" + CONTENT_URI + "/" + PATH_GROCERYLISTS;
+        public static final String CONTENT_ITEM_TYPE =
+                "vnd.android.cursor.item/" + CONTENT_URI + "/" + PATH_GROCERYLISTS;
+
+
+        public static final String TABLE_NAME = "grocerylists";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_LISTID = "groceryid";
+
+        public static Uri buildGroceryListsUri(long id){
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+    }
+
+    public static class GroceryProductEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_GROCERY).build();
+
+
+        public static final String CONTENT_TYPE =
+                "vnd.android.cursor.dir/" + CONTENT_URI + "/" + PATH_GROCERY;
+        public static final String CONTENT_ITEM_TYPE =
+                "vnd.android.cursor.item/" + CONTENT_URI + "/" + PATH_GROCERY;
+
+
+
+        public static final String TABLE_NAME = "grocery";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_QTY = "qty";
+
+        public static Uri buildGroceryProductUri(long id){
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+}
---
+++ OverripePantry/app/src/main/java/com/example/amanda/overripepantry/InventoryDBHelper.java
@@ -0,0 +1,86 @@
+/**
+ * Created by ffantl on 3/4/17.
+ */
+
+package com.example.amanda.overripepantry;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+/**
+ * Created by ffantl on 3/20/17.
+ */
+public class InventoryDBHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "inventory.db";
+    private static final String COLUMN_ID = "_id";
+
+    public InventoryDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * Called when the database is first created.
+     * @param db The database being created, which all SQL statements will be executed on.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        addProductTable(db);
+        addGroceryListsTable(db);
+        addGroceryProductTable(db);
+    }
+
+    /**
+            * Inserts the genre table into the database.
+     * @param db The SQLiteDatabase the table is being inserted into.
+     */
+//    CREATE TABLE IF NOT EXISTS products (
+//            productID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+//            title VARCHAR(255) NOT NULL,
+//    brand VARCHAR(255),
+//    weight FLOAT,
+//    upc VARCHAR(12),
+//    expDate DATE NOT NULL,
+//    entryDate DATE NOT NULL
+//    );
+
+    private void addProductTable(SQLiteDatabase db){
+        db.execSQL(
+                "CREATE TABLE " + InventoryContract.ProductEntry.TABLE_NAME + " (" +
+                        InventoryContract.ProductEntry._ID + " INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                        InventoryContract.ProductEntry.COLUMN_NAME_TITLE + " VARCHAR(255) NOT NULL, " +
+                        InventoryContract.ProductEntry.COLUMN_NAME_BRAND + " VARCHAR(255) NOT NULL, " +
+                        InventoryContract.ProductEntry.COLUMN_NAME_WEIGHT + " FLOAT, " +
+                        InventoryContract.ProductEntry.COLUMN_NAME_UPC + " VARCHAR(12), " +
+                        InventoryContract.ProductEntry.COLUMN_NAME_EXPDATE + " DATE, " +
+                        InventoryContract.ProductEntry.COLUMN_NAME_ENTRYDATE + " DATE );"
+        );
+    }
+
+    /**
+     * Inserts the movie table into the database.
+     * @param db The SQLiteDatabase the table is being inserted into.
+     */
+    private void addGroceryListsTable(SQLiteDatabase db){
+        db.execSQL(
+                "CREATE TABLE " + InventoryContract.GroceryListEntry.TABLE_NAME + " (" +
+                        InventoryContract.GroceryListEntry._ID + " INTEGER PRIMARY KEY, " +
+                        InventoryContract.GroceryListEntry.COLUMN_NAME_TITLE + " TEXT NOT NULL " +
+                        ");"
+        );
+    }
+
+    private void addGroceryProductTable(SQLiteDatabase db){
+        db.execSQL(
+                "CREATE TABLE " + InventoryContract.GroceryProductEntry.TABLE_NAME + " (" +
+                        InventoryContract.GroceryProductEntry._ID + " INTEGER PRIMARY KEY, " +
+                        InventoryContract.GroceryListEntry.COLUMN_NAME_TITLE + " TEXT NOT NULL, " +
+                        InventoryContract.GroceryProductEntry.COLUMN_NAME_QTY + " INT NOT NULL " +
+                        ");"
+        );
+    }
+
+}
